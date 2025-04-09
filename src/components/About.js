e8dd631a2d08883f250c59b30d6a5b9d0ef5e9e2
christopherchege <chrisshege35@gmail.com>
2025-04-09 03:17:30 +0300

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { image } from "../data/data";
 
+
+
 function About() {
-  return <div>About</div>;
+  return (
+   <div id="about">
+    <h2>About Me</h2>
+    <p>My name is Chris. I am a web developer with a passion for creating dynamic and responsive web applications.</p>
+    <img src={image} alt="I made this" />
+  </div>
+  );
 }
 
 export default About;
